8afd10f9f055eddb63a0e0213d55d715b3f27ec9
PyNix-Pro <rameenfar2013@gmail.com>
2026-09-26 15:44:21 +0530

    Added the code for 2nd lesson

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,2 +1,4 @@
-# Write your code below this line 👇
-print("Hello world!")
+#Both are different parts of the lesson try commenting a line with '#' to prevent or stop it from running to run one line only
+print("Hello world!\nHello world!\nHello world!")
+
+print("Hello" + " " + "Angela")
