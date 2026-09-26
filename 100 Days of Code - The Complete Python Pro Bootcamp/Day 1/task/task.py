eededbda137097570e8792f34242b35659bb161a
PyNix-Pro <rameenfar2013@gmail.com>
2026-09-26 16:32:30 +0530

    Added the input function along with print function

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,4 +1,1 @@
-#Both are different parts of the lesson try commenting a line with '#' to prevent or stop it from running to run one line only
-print("Hello world!\nHello world!\nHello world!")
-
-print("Hello" + " " + "Angela")
+print("Hello " + input("What is your name?") + "!")
